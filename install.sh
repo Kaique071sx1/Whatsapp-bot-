b3e4ba32dbe5d712916cf3968ae3ef2783be9527
Kaique071sx1 <kf12345678910z@gmail.com>
2025-07-15 10:57:11 -0300

    Refatora install.sh para apenas instalar dependências

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}Iniciando a instalação do Bot para WhatsApp...${NC}"
+echo -e "${GREEN}Iniciando a configuração do ambiente para o Bot...${NC}"
 
 # --- Atualizar pacotes do Termux ---
 echo -e "\n${YELLOW}Atualizando pacotes do sistema...${NC}"
@@ -14,11 +14,6 @@ apt update && apt upgrade -y
 # --- Instalar dependências essenciais (Node.js 18+, Git, etc.) ---
 echo -e "\n${YELLOW}Instalando Node.js, Git e outras dependências...${NC}"
 pkg install nodejs-lts git ffmpeg libwebp imagemagick -y
-
-# --- Clonar o repositório do bot (JÁ CORRIGIDO COM O SEU LINK) ---
-echo -e "\n${YELLOW}Clonando o repositório do bot...${NC}"
-git clone https://github.com/Kaique071sx1/Whatsapp-bot-.git
-cd Whatsapp-bot-
 
 # --- Instalar dependências do Node.js ---
 echo -e "\n${YELLOW}Instalando as dependências do projeto via npm...${NC}"
